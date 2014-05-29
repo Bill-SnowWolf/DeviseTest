6e7ab9c842d68406c0090f55ea0ff36af512de33
Bill Zhou <bill@micrometrics.ca>
2014-05-29 12:51:13 -0400

    Add devise gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseTest::Application.config.secret_token = 'abb555aa5d54881f2bfcf8bd06d7fbfa4b617874d8537581475b54da51e7c4ea8af106c666013ed86f3a126709be66b642ee36efc193e8254d93f7cebb830347'
